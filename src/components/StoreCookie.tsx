55c169eb3d90fc377d859d0a99726777fbe9bdf9
prushton2 <peterrushton418@gmail.com>
2023-12-02 19:18:26 -0500

    fixed linter errors

--- src/components/StoreCookie.tsx
+++ src/components/StoreCookie.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useCookies } from "react-cookie";   
 
 const StoreCookie = () => {
 
-    const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
+    const [cookies, setCookie] = useCookies(['token']);
 
     useEffect(() => {
+        cookies
         const queryParams = new URLSearchParams(window.location.search);
         const token = queryParams.get("token");
         setCookie("token", token, {expires: new Date(Date.now() + 604800000), httpOnly: true});
